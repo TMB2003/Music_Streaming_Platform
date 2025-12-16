a82670bb360ae875339b8a38777f554a279b0075
Taha <taha@Tahas-MacBook-Air.local>
2025-12-16 12:04:39 +0530

    Renaming: Camel Case Naming

--- user_service/dist/route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { loginUser, myProfile, registerUser } from "./controller.js";
-const router = express.Router();
-router.post("/user/register", registerUser);
-router.post("/user/login", loginUser);
-router.get("/user/profile", myProfile);
-export default router;
