3c557c24a0a57161a93c378b610c028534b2a92c
Taha <taha@Tahas-MacBook-Air.local>
2025-12-22 17:10:14 +0530

    Feat: Redis Connection Initiallization

--- songService/dist/index.js
+++ songService/dist/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import songRoutes from "./route.js";
+import { redisDB } from "./config/redisDB.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", songRoutes);
+redisDB.connect().then(() => console.log("Connected to Redis Successfully")).catch(console.error);
 const port = process.env['PORT'];
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
---
+++ songService/src/config/redisDB.ts
@@ -0,0 +1,11 @@
+import redis from "redis";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const redisDB = redis.createClient({
+    password: process.env['REDIS_PASSWORD']!,
+    socket: {
+        host: process.env['REDIS_HOST'],
+        port: Number(process.env['REDIS_PORT'])
+    }
+});
